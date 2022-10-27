a7f729afe527f08601129f8b9afa1ec8cd8f1df3
divine <divineobi07@gmail.com>
2022-10-27 17:11:09 +0100

    app component styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Header from "./components/Header/Header";
+import style from "./App.module.css";
 
 function App() {
-  return <div className="App">Vite</div>;
+  return (
+    <div className={style.mainContainer}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
