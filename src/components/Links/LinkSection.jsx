5990764749f361959af6b326afc5c060361aba0a
divine <divineobi07@gmail.com>
2022-11-03 12:19:45 +0100

    refactor: chnage Link name to DevLink

--- src/components/Links/LinkSection.jsx
+++ src/components/Links/LinkSection.jsx
@@ -1,4 +1,4 @@
-import Link from "./Link";
+import DevLink from "./Link";
 import style from "./LinkSection.module.css";
 import slack from "../../../public/assests/Img/slack.png";
 import github from "../../../public/assests/Icons/github.svg";
@@ -6,35 +6,39 @@ import github from "../../../public/assests/Icons/github.svg";
 const LinkSection = () => {
   return (
     <nav className={style.navigationLink}>
-      <Link url="https://twitter.com/Dhevine_0X" title="Twitter" id="twitter" />
-      <Link
+      <DevLink
+        url="https://twitter.com/Dhevine_0X"
+        title="Twitter"
+        id="twitter"
+      />
+      <DevLink
         id="btn__zuri"
         url="https://training.zuri.team/"
         title="Zuri Team"
       />
-      <Link
+      <DevLink
         id="books"
         url="http://books.zuri.team"
         title="Zuri Bookshop"
         info="Find more books on coding and design."
       />
-      <Link
+      <DevLink
         id="book__python"
         url="https://books.zuri.team/python-for-beginners?ref_id=Dalu"
         title="Python Books"
-        info='Check out books on python for both beginner and advanced.'
+        info="Check out books on python for both beginner and advanced."
       />
-      <Link
+      <DevLink
         id="pitch"
         url="https://background.zuri.team"
         title="Tech Talent Hunt"
-        info='Background checks on coders.'
+        info="Background checks on coders."
       />
-      <Link
+      <DevLink
         id="book__design"
         url="https://books.zuri.team/design-rules"
         title="Design Library"
-        info='Get a free design book here.'
+        info="Get a free design book here."
       />
 
       <div className={style.socialMediaLink}>
