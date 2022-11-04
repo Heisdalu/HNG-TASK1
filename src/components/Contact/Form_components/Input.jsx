7d24a3d5b412ef81f0c8b2c219973310a99e4bc9
divine <divineobi07@gmail.com>
2022-11-04 14:01:42 +0100

    fix: remove unused logs

--- src/components/Contact/Form_components/Input.jsx
+++ src/components/Contact/Form_components/Input.jsx
@@ -1,7 +1,6 @@
 import style from "./Input.module.css";
 
 const Input = ({ id, label, type, placeholder }) => {
-  console.log(style);
   return (
     <div className={`${style.inputBox} ${style[id]}`}>
       <label htmlFor={id} className={style.label}>
