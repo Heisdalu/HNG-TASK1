f8b4424eac2f72129b360f9a767f0098f2f07dd8
divine <divineobi07@gmail.com>
2022-11-04 13:58:40 +0100

    scroll to top

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -3,6 +3,7 @@ import style from "./ContactPage.module.css";
 import Form from "../../components/Contact/Form";
 
 const ContactPage = () => {
+  window.scrollTo(0, 0);
   return (
     <div className={style.formContainer}>
       <Form />
